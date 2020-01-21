3e2b9c8de5b46118b1300f5e770cd516037ff3e7
allansendagi <sendagiallan87@gmail.com>
2020-01-21 06:58:49 +0400

    adding logout functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 //Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types'
+import { logoutUser, getUserData } from './redux/actions/useractions';
 // import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
@@ -15,6 +17,7 @@ import jwtDecode from 'jwt-decode';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import axios from 'axios';
 
 //components
 import Navbar from './components/navbar';
@@ -22,16 +25,16 @@ import AuthRoute from './util/authroute';
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if(token){
-  const decodedToken = jwtDecode(token)
-  console.log(decodedToken)
+  const decodedToken = jwtDecode(token);
   if (decodedToken.exp*1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = '/login'
-    authenticated = false
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+     store.dispatch(getUserData())
   }
 }
 
@@ -45,8 +48,8 @@ class App extends Component {
       <div className='container'>
        <Switch>
         <Route exact path='/' component={home} />
-        <AuthRoute exact path='/login' component={login} authenticated={authenticated}/>
-        <AuthRoute exact path='/signup' component={signup} authenticated={authenticated}/>
+        <AuthRoute exact path='/login' component={login}/>
+        <AuthRoute exact path='/signup' component={signup}/>
        </Switch>
        </div>
       </Router>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
@@ -11,6 +10,10 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+//redux
+
+import { connect } from 'react-redux';
+import { signupUser } from '../redux/actions/useractions';
 
 const styles = (theme)=> ({
 	...theme.spreadIt
@@ -26,10 +29,14 @@ class signup extends Component {
 			password: '',
 			confirmPassword: '',
 			handle: '',
-			loading: false,
 			errors: {}
 		}
 	}
+	 componentWillReceiveProps(nextProps) {
+		if(nextProps.UI.errors){
+		this.setState({ errors: nextProps.UI.errors })
+	}
+}
 	handleSubmit = (event) => {
 		event.preventDefault();
 		this.setState({
@@ -41,21 +48,7 @@ class signup extends Component {
 			confirmPassword: this.state.confirmPassword,
 			handle: this.state.handle
 		}
-		axios.post('/signup', newUserData)
-			.then(res => {
-				console.log(res.data)
-				localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-				this.setState({
-					loading: false
-				})
-				this.props.history.push('/')
-			})
-			.catch(err => {
-				this.setState({
-					errors: err.response.data,
-					loading: false
-				})
-			})
+		this.props.signupUser(newUserData, this.props.history);
 }
 	handleChange = (event)=> {
 		this.setState({
@@ -63,8 +56,8 @@ class signup extends Component {
 		})
 	}
 	render() {
-		const {classes} = this.props;
-		const { errors, loading} = this.state;
+		const {classes, UI: { loading }} = this.props;
+		const { errors } = this.state;
 		return (
 		<Grid container className={classes.form}>
 		 <Grid item sm/>
@@ -150,7 +143,14 @@ class signup extends Component {
 	}
 }
 signup.propTypes = {
-	classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
+	UI: PropTypes.object.isRequired,
+	signupUser: PropTypes.func.isRequired
 }
+const mapStateToProps= (state) => ({
+	user: state.user,
+	UI: state.UI
+})
 
-export default withStyles(styles)(signup);
+export default connect(mapStateToProps, {signupUser})(withStyles(styles)(signup));
