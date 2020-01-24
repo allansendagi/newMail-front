7de0e67915b2e8cf04014eff258ee670c66754b5
allansendagi <sendagiallan87@gmail.com>
2020-01-24 07:13:38 +0400

    adding edit profile functionality

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './editdetails';
 
 //mui
 import Button from '@material-ui/core/Button';
@@ -21,6 +22,7 @@ import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
 import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 
 const styles =(theme) => ({
@@ -41,6 +43,9 @@ handleEditPicture = () => {
 	const fileInput = document.getElementById('imageInput');
 	 fileInput.click();
 }
+handleLogout = () => {
+	this.props.logoutUser()
+}
 	render() {
 		const { classes, user: {credentials:{ handle, createdAt, imageUrl, bio, website, location },
 		loading,
@@ -56,7 +61,7 @@ let profileMarkup = !loading ? (authenticated ? (
 	       type='file' 
 	       id='imageInput'
 	       hidden='hidden' 
-	       onChange={this.handleChange} />
+	       onChange={this.handleImageChange} />
 	      <Tooltip title='Edit profile picture' placement='top'>
 	       <IconButton onClick={this.handleEditPicture} className='button'>
 	         <EditIcon color='primary' />
@@ -89,8 +94,13 @@ let profileMarkup = !loading ? (authenticated ? (
 	   	)}
 	   	<CalendarToday color='primary'/>{''}
 	   	<span> Joined {dayjs(createdAt).format('MM YYYY')} </span>
-
 	   </div>
+	   <Tooltip title='logout' placement='top'>
+	     <IconButton onClick={this.handleLogout}>
+	      <KeyboardReturn color='primary'/>
+	     </IconButton>
+	   </Tooltip>
+	    <EditDetails/>
 	  </div>
 	</Paper>
 	) : (
